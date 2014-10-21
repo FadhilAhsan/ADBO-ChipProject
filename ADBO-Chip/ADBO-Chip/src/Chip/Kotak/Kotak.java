a50df4922ed5d3a42e983532609286db541c4cc5
i13003 <i13003@LAB02-19.ftis.unpar>
2014-10-21 12:50:48 +0700

    Class awal

---
+++ ADBO-Chip/ADBO-Chip/src/Chip/Kotak/Kotak.java
@@ -0,0 +1,16 @@
+package Chip.Kotak;
+
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author i13003
+ */
+public abstract class Kotak {
+    
+}
